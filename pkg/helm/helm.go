b1541c588603a3a3adc6fc165902ed5ae1afcef7
Paul B. Beskow <paul@beskow.no>
2020-08-14 14:50:03 +0200

    chore(helm): clean up API interface

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -6,7 +6,7 @@ package helm
 import (
 	"context"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,18 +15,14 @@ import (
 	"github.com/spf13/afero"
 	"gopkg.in/yaml.v2"
 	"helm.sh/helm/v3/pkg/release"
-	"helm.sh/helm/v3/pkg/strvals"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
 	"github.com/gofrs/flock"
-	"github.com/pkg/errors"
-
 	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart"
+	chartPkg "helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/cli/values"
-	"helm.sh/helm/v3/pkg/downloader"
+	valuesPkg "helm.sh/helm/v3/pkg/cli/values"
 	"helm.sh/helm/v3/pkg/getter"
 	"helm.sh/helm/v3/pkg/repo"
 )
@@ -37,36 +33,49 @@ const (
 	DefaultHelmDriver = "secrets"
 	// DefaultHelmLockExt is the extension used to create a file lock
 	DefaultHelmLockExt = ".lock"
+	// DefaultHelmLockTimeout is the default timeout in seconds
+	DefaultHelmLockTimeout = 30 * time.Second
 )
 
+// RepoAdder defines the interface for adding a helm repository
 type RepoAdder interface {
 	RepoAdd(repoName, url string) error
 }
 
+// RepoUpdater defines the interface for updating the helm repositories
 type RepoUpdater interface {
 	RepoUpdate() error
 }
 
-type ChartInstaller interface {
+// Installer defines the interface for installing a helm chart
+type Installer interface {
 	Install(releaseName, repoName, chartName string, args map[string]string) (*release.Release, error)
 }
 
+// Helmer defines all available helm operations
 type Helmer interface {
 	RepoAdder
 	RepoUpdater
-	ChartInstaller
+	Installer
 }
 
+// Helm stores all state required for running helm tasks
 type Helm struct {
 	restClient genericclioptions.RESTClientGetter
+	providers  getter.Providers
 	config     *Config
 	fs         *afero.Afero
 }
 
 // Config lists all configuration variables that must be set
+// to configure the environment for helm correctly
 type Config struct {
 	Namespace  string
 	KubeConfig string
+
+	// HomeDir allows us to modify where $HOME/.kube
+	// ends up
+	HomeDir string
 
 	HelmPluginsDirectory string
 	HelmRegistryConfig   string
@@ -74,7 +83,8 @@ type Config struct {
 	HelmRepositoryCache  string
 	HelmBaseDir          string
 
-	Debug bool
+	Debug       bool
+	DebugOutput io.Writer
 }
 
 // Envs returns the config as a helm compatible
@@ -82,6 +92,8 @@ type Config struct {
 func (c *Config) Envs() map[string]string {
 	return map[string]string{
 		"KUBECONFIG": c.KubeConfig,
+
+		"HOME": c.HomeDir,
 
 		"HELM_CACHE_HOME":        c.HelmBaseDir,
 		"HELM_CONFIG_HOME":       c.HelmBaseDir,
@@ -100,6 +112,12 @@ func (c *Config) Envs() map[string]string {
 func New(config *Config, fs *afero.Afero) *Helm {
 	return &Helm{
 		config: config,
+		providers: getter.Providers{
+			{
+				Schemes: []string{"https"},
+				New:     getter.NewHTTPGetter,
+			},
+		},
 		restClient: &genericclioptions.ConfigFlags{
 			KubeConfig: &config.KubeConfig,
 			Namespace:  &config.Namespace,
@@ -109,25 +127,26 @@ func New(config *Config, fs *afero.Afero) *Helm {
 }
 
 // RepoAdd adds repo with given name and url
+// nolint: funlen
 func (h *Helm) RepoAdd(name, url string) error {
 	restoreFn, err := EstablishEnv(h.config.Envs())
-	if err != nil {
-		return err
-	}
 
 	defer func() {
 		err = restoreFn()
 	}()
 
+	if err != nil {
+		return err
+	}
+
 	settings := cli.New()
-	repoFile := settings.RepositoryConfig
 
 	err = h.fs.MkdirAll(h.config.HelmBaseDir, 0744)
 	if err != nil {
 		return err
 	}
 
-	unlockFn, err := Lock(repoFile)
+	unlockFn, err := Lock(settings.RepositoryConfig)
 	if err != nil {
 		return err
 	}
@@ -136,7 +155,7 @@ func (h *Helm) RepoAdd(name, url string) error {
 		err = unlockFn()
 	}()
 
-	b, err := h.fs.ReadFile(repoFile)
+	b, err := h.fs.ReadFile(settings.RepositoryConfig)
 	if err != nil && !os.IsNotExist(err) {
 		return err
 	}
@@ -148,19 +167,18 @@ func (h *Helm) RepoAdd(name, url string) error {
 		return err
 	}
 
-	// We already have this repository, so we are done
 	if f.Has(name) {
 		return nil
 	}
 
-	c := repo.Entry{
+	entry := repo.Entry{
 		Name: name,
 		URL:  url,
 	}
 
-	r, err := repo.NewChartRepository(&c, getter.All(settings))
+	r, err := repo.NewChartRepository(&entry, h.providers)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	_, err = r.DownloadIndexFile()
@@ -168,9 +186,9 @@ func (h *Helm) RepoAdd(name, url string) error {
 		return err
 	}
 
-	f.Update(&c)
+	f.Update(&entry)
 
-	err = f.WriteFile(repoFile, 0644)
+	err = f.WriteFile(settings.RepositoryConfig, 0644)
 	if err != nil {
 		return err
 	}
@@ -181,18 +199,18 @@ func (h *Helm) RepoAdd(name, url string) error {
 // RepoUpdate updates charts for all helm repos
 func (h *Helm) RepoUpdate() error {
 	restoreFn, err := EstablishEnv(h.config.Envs())
-	if err != nil {
-		return err
-	}
 
 	defer func() {
 		err = restoreFn()
 	}()
 
-	settings := cli.New()
-	repoFile := settings.RepositoryConfig
+	if err != nil {
+		return err
+	}
 
-	exists, err := h.fs.Exists(repoFile)
+	settings := cli.New()
+
+	exists, err := h.fs.Exists(settings.RepositoryConfig)
 	if err != nil {
 		return err
 	}
@@ -201,20 +219,13 @@ func (h *Helm) RepoUpdate() error {
 		return nil
 	}
 
-	f, err := repo.LoadFile(repoFile)
+	f, err := repo.LoadFile(settings.RepositoryConfig)
 	if err != nil {
 		return err
 	}
 
-	providers := getter.Providers{
-		{
-			Schemes: []string{"https"},
-			New:     getter.NewHTTPGetter,
-		},
-	}
-
 	for _, cfg := range f.Repositories {
-		r, err := repo.NewChartRepository(cfg, providers)
+		r, err := repo.NewChartRepository(cfg, h.providers)
 		if err != nil {
 			return err
 		}
@@ -228,90 +239,101 @@ func (h *Helm) RepoUpdate() error {
 	return nil
 }
 
-// InstallChart
-func (h *Helm) Install(name, repo, chart string, args map[string]string) (*release.Release, error) {
+// InstallConfig defines the variables that must be set to install a chart
+type InstallConfig struct {
+	// ReleaseName is the name given to the release in Kubernetes
+	ReleaseName string
+	// Version is the version of the chart to install
+	Version string
+	// Chart is the name of the chart to install
+	Chart string
+	// Repo is the name of the repository from which to install
+	Repo string
+	// Values is a yaml encoded byte array of the values.yaml file
+	ValuesBody []byte
+}
+
+// RepoChart returns the [repo]/[chart] string
+func (c *InstallConfig) RepoChart() string {
+	return fmt.Sprintf("%s/%s", c.Repo, c.Chart)
+}
+
+// Install a chart, comparable to: https://helm.sh/docs/helm/helm_install/
+// though we have not implemented all the functionality found there
+// Some details to consider about CRDs:
+// - https://helm.sh/docs/chart_best_practices/custom_resource_definitions/#some-caveats-and-explanations
+// nolint: funlen
+func (h *Helm) Install(cfg *InstallConfig) (*release.Release, error) {
 	restoreFn, err := EstablishEnv(h.config.Envs())
-	if err != nil {
-		return nil, err
-	}
 
 	defer func() {
 		err = restoreFn()
 	}()
 
+	if err != nil {
+		return nil, err
+	}
+
 	settings := cli.New()
 
 	actionConfig := new(action.Configuration)
+
+	debug := func(format string, v ...interface{}) {
+		if h.config.Debug {
+			_, _ = fmt.Fprintf(h.config.DebugOutput, format, v...)
+		}
+	}
+
 	err = actionConfig.Init(h.restClient, settings.Namespace(), DefaultHelmDriver, debug)
 	if err != nil {
 		return nil, err
 	}
+
 	client := action.NewInstall(actionConfig)
 
-	if client.Version == "" && client.Devel {
-		client.Version = ">0.0.0-0"
-	}
-	//name, chart, err := client.NameAndChart(args)
-	client.ReleaseName = name
-	cp, err := client.ChartPathOptions.LocateChart(fmt.Sprintf("%s/%s", repo, chart), settings)
-	if err != nil {
-		return nil, err
-	}
-
-	p := getter.All(settings)
-	valueOpts := &values.Options{}
-
-	v, err := valueOpts.MergeValues(p)
-	if err != nil {
-		return nil, err
-	}
-
-	err = strvals.ParseInto(args["set"], v)
-	if err != nil {
-		return nil, err
-	}
-
-	// Check chart dependencies to make sure all are present in /charts
-	chartRequested, err := loader.Load(cp)
-	if err != nil {
-		return nil, err
-	}
-
-	validInstallableChart, err := isChartInstallable(chartRequested)
-	if err != nil {
-		return nil, err
-	}
-
-	if !validInstallableChart {
-		return nil, fmt.Errorf("invalid chart")
-	}
-
-	if req := chartRequested.Metadata.Dependencies; req != nil {
-		// If CheckDependencies returns an error, we have unfulfilled dependencies.
-		// As of Helm 2.4.0, this is treated as a stopping condition:
-		// https://github.com/helm/helm/issues/2209
-		if err := action.CheckDependencies(chartRequested, req); err != nil {
-			if client.DependencyUpdate {
-				man := &downloader.Manager{
-					Out:              os.Stderr,
-					ChartPath:        cp,
-					Keyring:          client.ChartPathOptions.Keyring,
-					SkipUpdate:       false,
-					Getters:          p,
-					RepositoryConfig: settings.RepositoryConfig,
-					RepositoryCache:  settings.RepositoryCache,
-				}
-				if err := man.Update(); err != nil {
-					return nil, err
-				}
-			} else {
-				return nil, err
-			}
-		}
-	}
-
+	client.Version = cfg.Version
+	client.ReleaseName = cfg.ReleaseName
 	client.Namespace = settings.Namespace()
-	r, err := client.Run(chartRequested, v)
+	client.CreateNamespace = true
+	client.Wait = true
+
+	cp, err := client.ChartPathOptions.LocateChart(cfg.RepoChart(), settings)
+	if err != nil {
+		return nil, err
+	}
+
+	valuesFile, err := StageValuesYaml(cfg.ValuesBody, h.fs)
+
+	defer func() {
+		_ = h.fs.Remove(valuesFile)
+	}()
+
+	if err != nil {
+		return nil, err
+	}
+
+	valueOpts := &valuesPkg.Options{
+		ValueFiles: []string{
+			valuesFile,
+		},
+	}
+
+	values, err := valueOpts.MergeValues(h.providers)
+	if err != nil {
+		return nil, err
+	}
+
+	chart, err := loader.Load(cp)
+	if err != nil {
+		return nil, err
+	}
+
+	err = IsChartInstallable(chart)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := client.Run(chart, values)
 	if err != nil {
 		return nil, err
 	}
@@ -319,17 +341,14 @@ func (h *Helm) Install(name, repo, chart string, args map[string]string) (*relea
 	return r, nil
 }
 
-func isChartInstallable(ch *chart.Chart) (bool, error) {
+// IsChartInstallable determines if a chart can be installed or not
+func IsChartInstallable(ch *chartPkg.Chart) error {
 	switch ch.Metadata.Type {
 	case "", "application":
-		return true, nil
+		return nil
+	default:
+		return fmt.Errorf("chart: %s is not installable", ch.Metadata.Type)
 	}
-	return false, errors.Errorf("%s charts are not installable", ch.Metadata.Type)
-}
-
-func debug(format string, v ...interface{}) {
-	format = fmt.Sprintf("[debug] %s\n", format)
-	_ = log.Output(2, fmt.Sprintf(format, v...))
 }
 
 // UnlockFn can be deferred in the calling function to
@@ -341,7 +360,7 @@ func Lock(file string) (UnlockFn, error) {
 	lockFile := strings.Replace(file, filepath.Ext(file), DefaultHelmLockExt, 1)
 	lock := flock.New(lockFile)
 
-	lockCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	lockCtx, cancel := context.WithTimeout(context.Background(), DefaultHelmLockTimeout)
 	defer cancel()
 
 	locked, err := lock.TryLockContext(lockCtx, 1*time.Second)
@@ -350,12 +369,31 @@ func Lock(file string) (UnlockFn, error) {
 	}
 
 	if locked {
-		return func() error {
-			return lock.Unlock()
-		}, nil
+		return lock.Unlock, nil
 	}
 
 	return nil, fmt.Errorf("failed to create lock: %s", lockFile)
+}
+
+// StageValuesYaml returns the path to the values.yaml
+// temporary file we have created
+func StageValuesYaml(body []byte, fs *afero.Afero) (string, error) {
+	f, err := fs.TempFile("", "values")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temporary file for values.yaml: %w", err)
+	}
+
+	_, err = f.Write(body)
+	if err != nil {
+		return "", fmt.Errorf("failed to write content to values.yaml: %w", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		return "", fmt.Errorf("failed to close values.yaml: %w", err)
+	}
+
+	return f.Name(), nil
 }
 
 // RestoreEnvFn can be deferred in the calling function
@@ -369,14 +407,7 @@ func EstablishEnv(envs map[string]string) (RestoreEnvFn, error) {
 	originalEnvVars := os.Environ()
 	os.Clearenv()
 
-	for key, val := range envs {
-		err := os.Setenv(key, val)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return func() error {
+	fn := func() error {
 		originalEnvVars := originalEnvVars
 
 		os.Clearenv()
@@ -392,7 +423,7 @@ func EstablishEnv(envs map[string]string) (RestoreEnvFn, error) {
 			case 1:
 				key = e[0]
 				val = ""
-			case 2:
+			case 2: // nolint: gomnd
 				key = e[0]
 				val = e[1]
 			}
@@ -404,5 +435,66 @@ func EstablishEnv(envs map[string]string) (RestoreEnvFn, error) {
 		}
 
 		return nil
+	}
+
+	for key, val := range envs {
+		err := os.Setenv(key, val)
+		if err != nil {
+			return fn, err
+		}
+	}
+
+	return fn, nil
+}
+
+// Chart contains the state for installing a chart
+type Chart struct {
+	RepositoryName string
+	RepositoryURL  string
+
+	ReleaseName string
+	Version     string
+	Chart       string
+
+	Values interface{}
+}
+
+// InstallConfig returns a valid install config
+func (c *Chart) InstallConfig() (*InstallConfig, error) {
+	values, err := yaml.Marshal(c.Values)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialise values: %w", err)
+	}
+
+	return &InstallConfig{
+		ReleaseName: c.ReleaseName,
+		Version:     c.Version,
+		Chart:       c.Chart,
+		Repo:        c.RepositoryName,
+		ValuesBody:  values,
 	}, nil
+}
+
+// MysqlValues demonstrates how the values can be set
+type MysqlValues struct {
+	MysqlRootPassword string           `yaml:"mysqlRootPassword"`
+	Persistence       MysqlPersistence `yaml:"persistence"`
+	ImagePullPolicy   string           `yaml:"imagePullPolicy"`
+}
+
+// MysqlPersistence demonstrates how the values can be set
+type MysqlPersistence struct {
+	Enabled bool `yaml:"enabled"`
+}
+
+// Mysql demonstrates how a chart can be specified
+func Mysql(values interface{}) *Chart {
+	return &Chart{
+		RepositoryName: "stable",
+		RepositoryURL:  "https://kubernetes-charts.storage.googleapis.com",
+		ReleaseName:    "mysql",
+		Version:        "1.6.6",
+		Chart:          "mysql",
+		Values:         values,
+	}
 }

eedb2c1a7314c275d3c559c67a90cb5e61535b80
Paul B. Beskow <paul@beskow.no>
2020-08-14 18:46:35 +0200

    chore(integration): ignore gosec on integration test

--- pkg/integration/integration.go
+++ pkg/integration/integration.go
@@ -158,7 +158,7 @@ func RandStringBytes(n int) string {
 
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		b[i] = letterBytes[rand.Intn(len(letterBytes))] //nolint: gosec
 	}
 
 	return string(b)

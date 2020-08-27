4125b75744fe5015fdf5fd5f48b594ef06bb511a
Paul B. Beskow <paul@beskow.no>
2020-08-27 09:42:45 +0200

    feat(ask): ask use if they have posted nameservers

---
+++ pkg/ask/ask.go
@@ -0,0 +1,82 @@
+// Package ask knows how to ask about stuff in the terminal
+package ask
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+)
+
+// Ask contains stating for asking stuff
+type Ask struct {
+	In  terminal.FileReader
+	Out terminal.FileWriter
+	Err io.Writer
+}
+
+// New returns an initialised ask
+func New() *Ask {
+	return &Ask{
+		In:  os.Stdin,
+		Out: os.Stdout,
+		Err: os.Stderr,
+	}
+}
+
+const message = `	We have created a domain for you at: %s
+
+	For this domain to resolve we need to delegate an
+	AWS HostedZone to your account, to do this we need
+	that you send us the domain and NS records that we
+	have printed below to one of these places:
+
+		- Slack: #kjøremiljø-support on slack
+		- Email: okctl@oslo.kommune.no
+		- Github issue: https://github.com/oslokommune/okctl/issues
+
+	----
+	Hi!
+
+	Please create a delegated zone for our domain:
+
+	Domain: %s
+	NameServers: %s
+
+	Thank you.
+    ----
+`
+
+// ConfirmPostingNameServers asks the user to confirm that they have posted the nameservers
+// to a channel where we can receive them
+func (a *Ask) ConfirmPostingNameServers(to io.Writer, domain string, nameServers []string) error {
+	_, err := fmt.Fprintf(to, message, domain, domain, strings.Join(nameServers, ","))
+	if err != nil {
+		return err
+	}
+
+	prompt := &survey.Confirm{
+		Message: "Have you sent us the information outlined above?",
+		Default: true,
+		Help:    "We have printed the name of your domain and name servers above, these need to be sent to use so we can create a delegated zone",
+	}
+
+	haveSent := false
+
+	err = survey.AskOne(prompt, &haveSent, survey.WithStdio(a.In, a.Out, a.Err))
+	if err != nil {
+		return err
+	}
+
+	if !haveSent {
+		_, err = fmt.Fprintf(to, "! You have not sent the domain and name server, your DNS records will not resolve until you do so")
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ pkg/ask/ask_test.go
@@ -0,0 +1,87 @@
+package ask_test
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/AlecAivazis/survey/v2/core"
+	"github.com/Netflix/go-expect"
+	"github.com/hinshun/vt10x"
+	"github.com/oslokommune/okctl/pkg/ask"
+	"github.com/sebdah/goldie/v2"
+	"github.com/stretchr/testify/require"
+)
+
+// This test is based on the tests from:
+// - https://github.com/AlecAivazis/survey
+// nolint: funlen
+func TestConfirmPostingNameServers(t *testing.T) {
+	// disable color output for all prompts to simplify testing
+	core.DisableColor = true
+
+	testCases := []struct {
+		name        string
+		domain      string
+		nameServers []string
+		golden      string
+		procedure   func(console *expect.Console, state *vt10x.State)
+	}{
+		{
+			name:        "Confirmed",
+			domain:      "test.oslo.systems",
+			nameServers: []string{"ns1.something.com", "ns2.something.com"},
+			procedure: func(c *expect.Console, _ *vt10x.State) {
+				_, _ = c.ExpectString("? Have you sent us the information outlined above? [? for help] (Y/n)")
+				_, _ = c.SendLine("")
+				_, _ = c.ExpectEOF()
+			},
+			golden: "message",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			c, state, err := vt10x.NewVT10XConsole(
+				expect.WithStdout(buf),
+				// Uncomment this line to get debug output:
+				expect.WithLogger(log.New(os.Stdout, "state", log.LstdFlags)),
+			)
+			require.Nil(t, err)
+			defer func() {
+				_ = c.Close()
+			}()
+
+			donec := make(chan struct{})
+			go func() {
+				defer close(donec)
+				tc.procedure(c, state)
+			}()
+
+			a := ask.New()
+			a.In = c.Tty()
+			a.Err = c.Tty()
+			a.Out = c.Tty()
+
+			msgBuf := new(bytes.Buffer)
+			err = a.ConfirmPostingNameServers(msgBuf, tc.domain, tc.nameServers)
+			require.Nil(t, err)
+
+			g := goldie.New(t)
+			g.Assert(t, tc.golden, msgBuf.Bytes())
+
+			// Close the slave end of the pty, and read the remaining bytes from the master end.
+			_ = c.Tty().Close()
+			<-donec
+
+			t.Logf("Raw output: %q", buf.String())
+
+			// Dump the terminal's screen.
+			t.Logf("\n%s", expect.StripTrailingEmptyLines(state.String()))
+		})
+	}
+}

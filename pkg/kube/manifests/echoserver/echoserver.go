92a54c6c72ced270ef5f004b97ddf1e0d22cea75
Paul B. Beskow <paul@beskow.no>
2020-08-28 14:23:12 +0200

    feat(echoserver): add echoserver, can test ingress and service

--- pkg/kube/manifests/echoserver/echoserver.go
+++ pkg/kube/manifests/echoserver/echoserver.go
@@ -1,1 +1,81 @@
+// Package echoserver provides a simple echoserver
 package echoserver
+
+import (
+	"context"
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// EchoServer contains the state for apply the external-dns
+// manifests to kubernetes
+type EchoServer struct {
+	Namespace     string
+	Version       string
+	Replicas      int32
+	ContainerPort int32
+	Ctx           context.Context
+}
+
+// New returns an initialised nginx deployment
+func New(namespace string) *EchoServer {
+	return &EchoServer{
+		Namespace:     namespace,
+		Version:       "1.10",
+		ContainerPort: 8080, // nolint: gomnd
+		Replicas:      1,
+		Ctx:           context.Background(),
+	}
+}
+
+// CreateDeployment creates the echoserver deployment manifest
+func (e *EchoServer) CreateDeployment(clientSet kubernetes.Interface) (interface{}, error) {
+	deployClient := clientSet.AppsV1().Deployments(e.Namespace)
+	return deployClient.Create(e.Ctx, e.DeploymentManifest(), metav1.CreateOptions{})
+}
+
+// DeploymentManifest returns the deployment manifest
+func (e *EchoServer) DeploymentManifest() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "echoserver",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &e.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "echoserver",
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "echoserver",
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:            "echoserver",
+							Image:           fmt.Sprintf("gcr.io/google-containers/echoserver:%s", e.Version),
+							ImagePullPolicy: "Always",
+							Ports: []v1.ContainerPort{
+								{
+									ContainerPort: e.ContainerPort,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
--- pkg/kube/manifests/echoserver/echoserver_test.go
+++ pkg/kube/manifests/echoserver/echoserver_test.go
@@ -1,1 +1,77 @@
-package echoserver
+package echoserver_test
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/oslokommune/okctl/pkg/integration"
+	"github.com/oslokommune/okctl/pkg/kube"
+	"github.com/oslokommune/okctl/pkg/kube/manifests/echoserver"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEchoServer(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping echoserver kube integration tests in short mode")
+	}
+
+	dir, err := ioutil.TempDir("", "echoserver")
+	assert.NoError(t, err)
+
+	defer func() {
+		_ = os.RemoveAll(dir)
+	}()
+
+	cluster := integration.NewKubernetesCluster()
+
+	err = cluster.Create(2 * time.Minute)
+
+	defer func() {
+		err := cluster.Cleanup()
+		assert.NoError(t, err)
+	}()
+
+	assert.NoError(t, err)
+
+	kubeConfPath, err := cluster.KubeConfig()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name string
+		ext  *echoserver.EchoServer
+	}{
+		{
+			name: "Should work",
+			ext:  echoserver.New("default"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			k, err := kube.New(kubeConfPath)
+			assert.NoError(t, err)
+
+			k.WithLogger(logrus.StandardLogger())
+
+			resources, err := k.Apply(tc.ext.CreateDeployment)
+			assert.NoError(t, err)
+
+			err = k.Watch(resources, 2*time.Minute)
+			assert.NoError(t, err)
+
+			outputs, err := cluster.Debug("default")
+			assert.NoError(t, err)
+
+			for title, data := range outputs {
+				log.Println(title)
+				log.Println(data)
+			}
+		})
+	}
+}

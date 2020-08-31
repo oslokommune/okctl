72fe229ca8ffc3628085eda2ce37b60a7d42eb28
Paul B. Beskow <paul@beskow.no>
2020-08-31 19:30:49 +0200

    feat(github): add functionality for fetching teams, repos, and create deploy key

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -1,36 +1,139 @@
 // Package github provides a client for interacting with the Github API
 package github
 
-import "github.com/oslokommune/okctl/pkg/credentials/github"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v32/github"
+	githubAuth "github.com/oslokommune/okctl/pkg/credentials/github"
+	"golang.org/x/oauth2"
+)
+
+// DefaultOrg is the default organisation used with okctl
+const DefaultOrg = "oslokommune"
 
 // Github contains the state for interacting with the github API
 type Github struct {
-	auth github.Authenticator
+	Organisation string
+	Ctx          context.Context
+	Client       *github.Client
 }
+
+// Repository shadows github.Repository
+type Repository = github.Repository
+
+// Team shadows github.Team
+type Team = github.Team
+
+// Key shadows github.Key
+type Key = github.Key
 
 // New returns an initialised github API client
-func New(auth github.Authenticator) *Github {
-	return &Github{
-		auth: auth,
+func New(org string, auth githubAuth.Authenticator) (*Github, error) {
+	ctx := context.Background()
+
+	credentials, err := auth.Raw()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get github credentials: %w", err)
 	}
+
+	client := github.NewClient(
+		oauth2.NewClient(ctx,
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: credentials.AccessToken,
+				},
+			),
+		),
+	)
+
+	return &Github{
+		Organisation: org,
+		Ctx:          ctx,
+		Client:       client,
+	}, nil
 }
 
-// Teams fetches all teams within the oslokommune org
-func (g *Github) Teams() error {
-	return nil
+// Teams fetches all teams within the given organisation
+func (g *Github) Teams() ([]*Team, error) {
+	opts := &github.ListOptions{
+		PerPage: 10, // nolint: gomnd
+	}
+
+	var allTeams []*github.Team
+
+	for {
+		teams, resp, err := g.Client.Teams.ListTeams(g.Ctx, g.Organisation, opts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve teams: %w", err)
+		}
+
+		allTeams = append(allTeams, teams...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+
+		opts.Page = resp.NextPage
+	}
+
+	return allTeams, nil
 }
 
-// Repositories fetches all the repositories
-func (g *Github) Repositories() error {
-	return nil
+// Repositories fetches all the repositories within the given organisation
+func (g *Github) Repositories() ([]*Repository, error) {
+	opts := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 10, // nolint: gomnd
+		},
+	}
+
+	var allRepos []*github.Repository
+
+	for {
+		repos, resp, err := g.Client.Repositories.ListByOrg(g.Ctx, g.Organisation, opts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve repositories: %w", err)
+		}
+
+		allRepos = append(allRepos, repos...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+
+		opts.Page = resp.NextPage
+	}
+
+	return allRepos, nil
 }
 
-// DeployKey ...
-func (g *Github) DeployKey() error {
-	return nil
+// CreateDeployKey creates a read-only deploy key for the given owner/repo
+func (g *Github) CreateDeployKey(repository, title, publicKey string) (*Key, error) {
+	key, _, err := g.Client.Repositories.CreateKey(g.Ctx, g.Organisation, repository, &github.Key{
+		Title:    &title,
+		Key:      &publicKey,
+		ReadOnly: BoolPtr(true),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create deploy key: %w", err)
+	}
+
+	return key, nil
 }
 
-// RemoteIsThis ...
-func (g *Github) RemoteIsThis(repo string) error {
-	return nil
+// BoolPtr returns a pointer to the bool
+func BoolPtr(v bool) *bool {
+	return &v
+}
+
+// StringPtr returns a pointer to the string
+func StringPtr(v string) *string {
+	return &v
+}
+
+// Int64Ptr returns a pointer to the int64
+func Int64Ptr(v int64) *int64 {
+	return &v
 }
---
+++ pkg/github/github_test.go
@@ -0,0 +1,156 @@
+package github_test
+
+import (
+	"net/http"
+	"testing"
+
+	"gopkg.in/h2non/gock.v1"
+
+	ghPkg "github.com/google/go-github/v32/github"
+	githubAuth "github.com/oslokommune/okctl/pkg/credentials/github"
+	"github.com/oslokommune/okctl/pkg/github"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGithubRepositories(t *testing.T) {
+	repositories := []*ghPkg.Repository{
+		{
+			ID:      github.Int64Ptr(12345),
+			Name:    github.StringPtr("something"),
+			Private: github.BoolPtr(true),
+		},
+	}
+
+	testCases := []struct {
+		name   string
+		github *github.Github
+		expect []*github.Repository
+	}{
+		{
+			name: "Should work",
+			github: func() *github.Github {
+				gh, err := github.New(github.DefaultOrg, githubAuth.New(githubAuth.NewInMemoryPersister(), &http.Client{}, githubAuth.NewAuthStatic(&githubAuth.Credentials{
+					AccessToken: "meh",
+					Type:        githubAuth.CredentialsTypePersonalAccessToken,
+				})))
+				assert.NoError(t, err)
+
+				return gh
+			}(),
+			expect: repositories,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			defer gock.Off()
+
+			gock.New(tc.github.Client.BaseURL.String()).
+				Get("/orgs/oslokommune/repos").
+				MatchParam("per_page", "10").
+				Reply(http.StatusOK).
+				JSON(repositories)
+
+			got, err := tc.github.Repositories()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expect, got)
+		})
+	}
+}
+
+func TestGithubTeams(t *testing.T) {
+	teams := []*ghPkg.Team{
+		{
+			ID:      github.Int64Ptr(12345),
+			Name:    github.StringPtr("myTeam"),
+			Privacy: github.StringPtr("secret"),
+		},
+	}
+
+	testCases := []struct {
+		name   string
+		github *github.Github
+		expect []*github.Team
+	}{
+		{
+			name: "Should work",
+			github: func() *github.Github {
+				gh, err := github.New(github.DefaultOrg, githubAuth.New(githubAuth.NewInMemoryPersister(), &http.Client{}, githubAuth.NewAuthStatic(&githubAuth.Credentials{
+					AccessToken: "meh",
+					Type:        githubAuth.CredentialsTypePersonalAccessToken,
+				})))
+				assert.NoError(t, err)
+
+				return gh
+			}(),
+			expect: teams,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			defer gock.Off()
+
+			gock.New(tc.github.Client.BaseURL.String()).
+				Get("/orgs/oslokommune/teams").
+				MatchParam("per_page", "10").
+				Reply(http.StatusOK).
+				JSON(teams)
+
+			got, err := tc.github.Teams()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expect, got)
+		})
+	}
+}
+
+func TestGithubCreateDeployKey(t *testing.T) {
+	key := &ghPkg.Key{
+		ID:       github.Int64Ptr(12345),
+		Key:      github.StringPtr("ssh-rsa 1234567abc"),
+		URL:      github.StringPtr("https://"),
+		Title:    github.StringPtr("myTitle"),
+		ReadOnly: github.BoolPtr(true),
+	}
+
+	testCases := []struct {
+		name   string
+		github *github.Github
+		expect *github.Key
+	}{
+		{
+			name: "Should work",
+			github: func() *github.Github {
+				gh, err := github.New(github.DefaultOrg, githubAuth.New(githubAuth.NewInMemoryPersister(), &http.Client{}, githubAuth.NewAuthStatic(&githubAuth.Credentials{
+					AccessToken: "meh",
+					Type:        githubAuth.CredentialsTypePersonalAccessToken,
+				})))
+				assert.NoError(t, err)
+
+				return gh
+			}(),
+			expect: key,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			defer gock.Off()
+
+			gock.New(tc.github.Client.BaseURL.String()).
+				Post("/repos/oslokommune/myRepo/keys").
+				Reply(http.StatusOK).
+				JSON(key)
+
+			got, err := tc.github.CreateDeployKey("myRepo", "myTitle", "ssh-rsa 1234567abc")
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expect, got)
+		})
+	}
+}

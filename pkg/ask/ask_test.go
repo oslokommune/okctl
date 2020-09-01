e6b16fb712d1f4768ba7df68c3f196eecf4d060b
Paul B. Beskow <paul@beskow.no>
2020-09-01 10:16:13 +0200

    feat(ask): create oauth app helper

--- pkg/ask/ask_test.go
+++ pkg/ask/ask_test.go
@@ -166,3 +166,160 @@ func TestAskSelectInfrastructureRepository(t *testing.T) {
 		})
 	}
 }
+
+// nolint: funlen lll
+func TestAskSelectTeam(t *testing.T) {
+	// disable color output for all prompts to simplify testing
+	core.DisableColor = true
+
+	testCases := []struct {
+		name      string
+		teams     []*github.Team
+		procedure func(console *expect.Console, state *vt10x.State)
+		expect    interface{}
+	}{
+		{
+			name: "Selected",
+			teams: []*github.Team{
+				{
+					Name: github.StringPtr("Super team"),
+				},
+				{
+					Name: github.StringPtr("Awesome team"),
+				},
+			},
+			procedure: func(c *expect.Console, state *vt10x.State) {
+				_, _ = c.ExpectString("? Select team that is authorised to access the Argo CD UI:  [Use arrows to move, type to filter, ? for more help]\r\n> Super team\r\n  Awesome team\r\n")
+				_, _ = c.SendLine("")
+				_, _ = c.ExpectEOF()
+			},
+			expect: &github.Team{
+				Name: github.StringPtr("Super team"),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			c, state, err := vt10x.NewVT10XConsole(
+				expect.WithStdout(buf),
+				// Uncomment this line to get debug output:
+				// expect.WithLogger(log.New(os.Stdout, "state", log.LstdFlags)),
+			)
+			require.Nil(t, err)
+			defer func() {
+				_ = c.Close()
+			}()
+
+			donec := make(chan struct{})
+			go func() {
+				defer close(donec)
+				tc.procedure(c, state)
+			}()
+
+			a := ask.New()
+			a.In = c.Tty()
+			a.Err = c.Tty()
+			a.Out = c.Tty()
+
+			got, err := a.SelectTeam(tc.teams)
+			require.Nil(t, err)
+			assert.Equal(t, tc.expect, got)
+
+			// Close the slave end of the pty, and read the remaining bytes from the master end.
+			_ = c.Tty().Close()
+			<-donec
+
+			t.Logf("Raw output: %q", buf.String())
+
+			// Dump the terminal's screen.
+			t.Logf("Lines: \n%s", expect.StripTrailingEmptyLines(state.String()))
+		})
+	}
+}
+
+// nolint: funlen lll
+func TestAskCreateOauthApp(t *testing.T) {
+	// disable color output for all prompts to simplify testing
+	core.DisableColor = true
+
+	testCases := []struct {
+		name      string
+		golden    string
+		opts      ask.OauthAppOpts
+		procedure func(console *expect.Console, state *vt10x.State)
+		expect    *ask.OauthApp
+	}{
+		{
+			name: "Confirmed",
+			opts: ask.OauthAppOpts{
+				Organisation: "oslokommune",
+				Name:         "okctl",
+				URL:          "https://something",
+				CallbackURL:  "https://something/callback",
+			},
+			procedure: func(c *expect.Console, _ *vt10x.State) {
+				_, _ = c.ExpectString("? Attempt to open browser window to github oauth apps? [? for help] (Y/n)")
+				_, _ = c.SendLine("n")
+				_, _ = c.ExpectString("? Enter Client ID of the oauth app:")
+				_, _ = c.SendLine("client_id")
+				_, _ = c.ExpectString("? Enter Client Secret of the oauth app:")
+				_, _ = c.SendLine("client_secret")
+				_, _ = c.ExpectEOF()
+			},
+			golden: "oauth-message",
+			expect: &ask.OauthApp{
+				ClientID:     "client_id",
+				ClientSecret: "client_secret",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			c, state, err := vt10x.NewVT10XConsole(
+				expect.WithStdout(buf),
+				// Uncomment this line to get debug output:
+				// expect.WithLogger(log.New(os.Stdout, "state", log.LstdFlags)),
+			)
+			require.Nil(t, err)
+			defer func() {
+				_ = c.Close()
+			}()
+
+			donec := make(chan struct{})
+			go func() {
+				defer close(donec)
+				tc.procedure(c, state)
+			}()
+
+			a := ask.New()
+			a.In = c.Tty()
+			a.Err = c.Tty()
+			a.Out = c.Tty()
+
+			msgBuf := new(bytes.Buffer)
+			got, err := a.CreateOauthApp(msgBuf, tc.opts)
+			require.Nil(t, err)
+			assert.Equal(t, tc.expect, got)
+
+			g := goldie.New(t)
+			g.Assert(t, tc.golden, msgBuf.Bytes())
+
+			// Close the slave end of the pty, and read the remaining bytes from the master end.
+			_ = c.Tty().Close()
+			<-donec
+
+			t.Logf("Raw output: %q", buf.String())
+
+			// Dump the terminal's screen.
+			t.Logf("\n%s", expect.StripTrailingEmptyLines(state.String()))
+		})
+	}
+}

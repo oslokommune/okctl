d7b6512075b6e94e8ad4893460d277a2e799d7d8
Paul B. Beskow <paul@beskow.no>
2020-09-01 08:45:17 +0200

    feat(ask): select infrastructure repository

--- pkg/ask/ask.go
+++ pkg/ask/ask.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/oslokommune/okctl/pkg/github"
+
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/AlecAivazis/survey/v2/terminal"
 )
@@ -27,7 +29,7 @@ func New() *Ask {
 	}
 }
 
-const message = `	We have created a domain for you at: %s
+const nameServersMsg = `	We have created a domain for you at: %s
 
 	For this domain to resolve we need to delegate an
 	AWS HostedZone to your account, to do this we need
@@ -52,7 +54,7 @@ const message = `	We have created a domain for you at: %s
 // ConfirmPostingNameServers asks the user to confirm that they have posted the nameservers
 // to a channel where we can receive them
 func (a *Ask) ConfirmPostingNameServers(to io.Writer, domain string, nameServers []string) error {
-	_, err := fmt.Fprintf(to, message, domain, domain, strings.Join(nameServers, ","))
+	_, err := fmt.Fprintf(to, nameServersMsg, domain, domain, strings.Join(nameServers, ","))
 	if err != nil {
 		return err
 	}
@@ -78,4 +80,69 @@ func (a *Ask) ConfirmPostingNameServers(to io.Writer, domain string, nameServers
 	}
 
 	return nil
+}
+
+const iacRepoMsg = `	The github repository that you select will
+	be used as the infrastructure as code (IAC) repository. 
+
+	Requirements:
+
+		- Must be a private repository
+		- Must be in oslokommune organisation
+		- Should be the same repository as this
+	
+	It should be the same repository as you are in now, but you can 
+	override this if you require so. Argo CD will be setup so it can 
+	read the contents of this IAC repository and looks for new Kubernetes
+	manifests for the deployments it is watching.
+
+	If you select a different repository, please ensure that you
+	move the outputs and .okctl.yml to that location.
+`
+
+// SelectInfrastructureRepository queries the user to select a repository as their IAC repository
+func (a *Ask) SelectInfrastructureRepository(defaultRepo string, repos []*github.Repository) (*github.Repository, error) {
+	keys := make([]string, len(repos))
+	mappedRepos := make(map[string]*github.Repository, len(repos))
+
+	for i, r := range repos {
+		mappedRepos[*r.FullName] = r
+		keys[i] = *r.FullName
+	}
+
+	repo := ""
+
+	prompt := &survey.Select{
+		Message: "Select repository that Argo CD will use for infrastructure as code:",
+		Options: keys,
+		Help:    iacRepoMsg,
+		Default: defaultRepo,
+	}
+
+	isPrivateFn := func(val interface{}) error {
+		s, ok := val.(survey.OptionAnswer)
+		if !ok {
+			return fmt.Errorf("not an option answer")
+		}
+
+		private := *mappedRepos[s.Value].Private
+		if !private {
+			return fmt.Errorf("repository must be private")
+		}
+
+		return nil
+	}
+
+	err := survey.AskOne(
+		prompt,
+		&repo,
+		survey.WithValidator(survey.Required),
+		survey.WithValidator(isPrivateFn),
+		survey.WithStdio(a.In, a.Out, a.Err),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed while asking user to select repo: %w", err)
+	}
+
+	return mappedRepos[repo], nil
 }
--- pkg/ask/ask_test.go
+++ pkg/ask/ask_test.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/bmizerany/assert"
+
+	"github.com/oslokommune/okctl/pkg/github"
+
 	"github.com/AlecAivazis/survey/v2/core"
 	"github.com/Netflix/go-expect"
 	"github.com/hinshun/vt10x"
@@ -80,6 +84,85 @@ func TestConfirmPostingNameServers(t *testing.T) {
 
 			// Dump the terminal's screen.
 			t.Logf("\n%s", expect.StripTrailingEmptyLines(state.String()))
+		})
+	}
+}
+
+// nolint: funlen lll
+func TestAskSelectInfrastructureRepository(t *testing.T) {
+	// disable color output for all prompts to simplify testing
+	core.DisableColor = true
+
+	testCases := []struct {
+		name      string
+		def       string
+		repos     []*github.Repository
+		procedure func(console *expect.Console, state *vt10x.State)
+		expect    interface{}
+	}{
+		{
+			name: "Selected",
+			def:  "oslokommune/else",
+			repos: []*github.Repository{
+				{
+					FullName: github.StringPtr("oslokommune/something"),
+					Private:  github.BoolPtr(true),
+				},
+				{
+					FullName: github.StringPtr("oslokommune/else"),
+					Private:  github.BoolPtr(true),
+				},
+			},
+			procedure: func(c *expect.Console, state *vt10x.State) {
+				_, _ = c.ExpectString("? Select repository that Argo CD will use for infrastructure as code:  [Use arrows to move, type to filter, ? for more help]\r\n  oslokommune/something\r\n> oslokommune/else\r\n")
+				_, _ = c.SendLine("")
+				_, _ = c.ExpectEOF()
+			},
+			expect: &github.Repository{
+				FullName: github.StringPtr("oslokommune/else"),
+				Private:  github.BoolPtr(true),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			c, state, err := vt10x.NewVT10XConsole(
+				expect.WithStdout(buf),
+				// Uncomment this line to get debug output:
+				// expect.WithLogger(log.New(os.Stdout, "state", log.LstdFlags)),
+			)
+			require.Nil(t, err)
+			defer func() {
+				_ = c.Close()
+			}()
+
+			donec := make(chan struct{})
+			go func() {
+				defer close(donec)
+				tc.procedure(c, state)
+			}()
+
+			a := ask.New()
+			a.In = c.Tty()
+			a.Err = c.Tty()
+			a.Out = c.Tty()
+
+			got, err := a.SelectInfrastructureRepository(tc.def, tc.repos)
+			require.Nil(t, err)
+			assert.Equal(t, tc.expect, got)
+
+			// Close the slave end of the pty, and read the remaining bytes from the master end.
+			_ = c.Tty().Close()
+			<-donec
+
+			t.Logf("Raw output: %q", buf.String())
+
+			// Dump the terminal's screen.
+			t.Logf("Lines: \n%s", expect.StripTrailingEmptyLines(state.String()))
 		})
 	}
 }

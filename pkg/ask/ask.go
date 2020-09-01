d4732639a8ab6a291d71527d91161f453f27e988
Paul B. Beskow <paul@beskow.no>
2020-09-01 10:16:13 +0200

    feat(ask): create oauth app helper

--- pkg/ask/ask.go
+++ pkg/ask/ask.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/pkg/browser"
+
 	"github.com/oslokommune/okctl/pkg/github"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -82,7 +84,7 @@ func (a *Ask) ConfirmPostingNameServers(to io.Writer, domain string, nameServers
 	return nil
 }
 
-const iacRepoMsg = `	The github repository that you select will
+const iacHelp = `	The github repository that you select will
 	be used as the infrastructure as code (IAC) repository. 
 
 	Requirements:
@@ -115,7 +117,7 @@ func (a *Ask) SelectInfrastructureRepository(defaultRepo string, repos []*github
 	prompt := &survey.Select{
 		Message: "Select repository that Argo CD will use for infrastructure as code:",
 		Options: keys,
-		Help:    iacRepoMsg,
+		Help:    iacHelp,
 		Default: defaultRepo,
 	}
 
@@ -145,4 +147,145 @@ func (a *Ask) SelectInfrastructureRepository(defaultRepo string, repos []*github
 	}
 
 	return mappedRepos[repo], nil
+}
+
+const teamHelp = `	The team you select will be the only one
+	that is allowed to login to the Argo CD UI using Github
+	as the authorisation provider.`
+
+// SelectTeam queries the user to select a team for authorisation towards argo cd
+func (a *Ask) SelectTeam(teams []*github.Team) (*github.Team, error) {
+	keys := make([]string, len(teams))
+	mappedTeams := make(map[string]*github.Team, len(teams))
+
+	for i, r := range teams {
+		mappedTeams[r.GetName()] = r
+		keys[i] = r.GetName()
+	}
+
+	repo := ""
+
+	prompt := &survey.Select{
+		Message: "Select team that is authorised to access the Argo CD UI:",
+		Options: keys,
+		Help:    teamHelp,
+	}
+
+	err := survey.AskOne(
+		prompt,
+		&repo,
+		survey.WithValidator(survey.Required),
+		survey.WithStdio(a.In, a.Out, a.Err),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed while asking user to select team: %w", err)
+	}
+
+	return mappedTeams[repo], nil
+}
+
+const oauthAppMsg = `	We will now be creating an oauth application
+	for authorising access to a UI. Below you will find the
+	the steps to follow and information you will need to fill
+	in:
+
+	1. Open the following URL: https://github.com/settings/developers
+	   or answer Yes to the terminal query.
+
+	2. Click on "Register a new application"
+
+	3. Fill in the fields with the information below:
+
+		Application name: '%s'
+		Homepage URL: '%s'
+		Application description: '%s'
+		Authorization callback URL: '%s'
+
+	4. Click on "Register application"
+
+	5. Respond to the terminal queries for the "Client ID" and
+	   "Client Secret"
+
+	6. Click on the "Transfer Ownership" button
+
+	7. Enter the following information:
+
+		Type the name of the application to confirm: '%s'
+		New owner’s GitHub username or organization name: '%s'
+
+	8. Click on "Transfer this application"
+
+`
+
+const oauthAppHelp = `	We will be creating an oauth app that authorises
+	those who have access to a given UI. It is not possible to
+	automate this task, and therefore it requires some manual
+	work.
+`
+
+// OauthApp contains the oauth app state
+type OauthApp struct {
+	ClientID     string
+	ClientSecret string
+}
+
+// OauthAppOpts contains the inputs
+type OauthAppOpts struct {
+	Organisation string
+	Name         string
+	URL          string
+	CallbackURL  string
+}
+
+// CreateOauthApp helps the user create and transfer and oauth app
+func (a *Ask) CreateOauthApp(to io.Writer, opts OauthAppOpts) (*OauthApp, error) {
+	_, err := fmt.Fprintf(to, oauthAppMsg, opts.Name, opts.URL, opts.Name, opts.CallbackURL, opts.Name, opts.Organisation)
+	if err != nil {
+		return nil, err
+	}
+
+	prompt := &survey.Confirm{
+		Message: "Attempt to open browser window to github oauth apps?",
+		Default: true,
+		Help:    oauthAppHelp,
+	}
+
+	doOpen := false
+
+	err = survey.AskOne(prompt, &doOpen, survey.WithStdio(a.In, a.Out, a.Err))
+	if err != nil {
+		return nil, err
+	}
+
+	if doOpen {
+		_ = browser.OpenURL("https://github.com/settings/developers")
+	}
+
+	qs := []*survey.Question{
+		{
+			Name:     "clientid",
+			Prompt:   &survey.Input{Message: "Enter Client ID of the oauth app:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "clientsecret",
+			Prompt:   &survey.Input{Message: "Enter Client Secret of the oauth app:"},
+			Validate: survey.Required,
+		},
+	}
+
+	answers := struct {
+		ClientID     string
+		ClientSecret string
+	}{}
+
+	err = survey.Ask(qs, &answers, survey.WithStdio(a.In, a.Out, a.Err))
+	if err != nil {
+		return nil, err
+	}
+
+	return &OauthApp{
+		ClientID:     answers.ClientID,
+		ClientSecret: answers.ClientSecret,
+	}, nil
 }

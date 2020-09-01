6f1d80affdc38250e23826a7c0d9a98771e4631c
Paul B. Beskow <paul@beskow.no>
2020-09-01 07:41:46 +0200

    feat(git): some helpers for extracting stuff

---
+++ pkg/git/git.go
@@ -0,0 +1,38 @@
+// Package git implements some git helpers
+package git
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/go-git/go-git/v5"
+)
+
+// GithubRepoFullName attempts to extract the github repo name, returns
+// and empty string if it doesn't succeed
+func GithubRepoFullName(org, gitPath string) (string, error) {
+	g, err := git.PlainOpen(gitPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open git repository: %w", err)
+	}
+
+	remotes, err := g.Remotes()
+	if err != nil {
+		return "", fmt.Errorf("failed to get remotes: %w", err)
+	}
+
+	re := regexp.MustCompile(fmt.Sprintf(`(?U)(?:.*github\.com:)(?P<repo>%s/.*)(?:\.git)`, org))
+
+	for _, remote := range remotes {
+		matches := re.FindStringSubmatch(remote.String())
+		names := re.SubexpNames()
+
+		for i, name := range names {
+			if name == "repo" {
+				return matches[i], nil
+			}
+		}
+	}
+
+	return "", nil
+}

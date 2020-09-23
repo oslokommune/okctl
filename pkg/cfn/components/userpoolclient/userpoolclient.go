cf4eff6c10c6c7dafeb89191083253e2529882ab
Paul B. Beskow <paul@beskow.no>
2020-09-23 14:46:28 +0200

    feat(cfn): add cognito user pool client

--- pkg/cfn/components/userpoolclient/userpoolclient.go
+++ pkg/cfn/components/userpoolclient/userpoolclient.go
@@ -1,1 +1,92 @@
+// Package userpoolclient provides an implementation of Cognito UserPoolClient
+// - https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cognito-userpoolclient.html
 package userpoolclient
+
+import (
+	"fmt"
+
+	"github.com/oslokommune/okctl/pkg/cfn"
+
+	"github.com/awslabs/goformation/v4/cloudformation"
+	"github.com/awslabs/goformation/v4/cloudformation/cognito"
+)
+
+const (
+	refreshTokenValidityDays = 30
+)
+
+// UserPoolClient stores the state for a cloud formation
+// cognito user pool client
+type UserPoolClient struct {
+	StoredName  string
+	ClientName  string
+	CallBackURL string
+	Environment string
+	Repository  string
+	Purpose     string
+	UserPool    cfn.NameReferencer
+}
+
+// NamedOutputs returns the named outputs
+func (c *UserPoolClient) NamedOutputs() map[string]map[string]interface{} {
+	return cfn.NewValue(fmt.Sprintf("%sClientID", c.Purpose), c.Ref()).NamedOutputs()
+}
+
+// Resource returns the cloud formation resource for a
+// cognito user pool client
+func (c *UserPoolClient) Resource() cloudformation.Resource {
+	return &cognito.UserPoolClient{
+		AllowedOAuthFlows: []string{
+			// https://auth0.com/docs/flows/authorization-code-flow
+			"code",
+		},
+		AllowedOAuthFlowsUserPoolClient: true,
+		AllowedOAuthScopes: []string{
+			"email",
+			"openid",
+		},
+		CallbackURLs: []string{
+			c.CallBackURL,
+		},
+		ClientName: c.ClientName,
+		// https://tools.ietf.org/html/rfc6749#section-3.1.2
+		DefaultRedirectURI:         c.CallBackURL,
+		ExplicitAuthFlows:          nil, // ?
+		GenerateSecret:             true,
+		PreventUserExistenceErrors: "ENABLED",
+		ReadAttributes: []string{
+			"email",
+			"openid",
+		},
+		RefreshTokenValidity:       refreshTokenValidityDays,
+		SupportedIdentityProviders: nil,
+		UserPoolId:                 c.UserPool.Ref(),
+		AWSCloudFormationDependsOn: []string{
+			c.UserPool.Name(),
+		},
+	}
+}
+
+// Ref returns a cloud formation intrinsic ref to the resource
+func (c *UserPoolClient) Ref() string {
+	return cloudformation.Ref(c.Name())
+}
+
+// Name returns the logical identifier of the resource
+func (c *UserPoolClient) Name() string {
+	return c.StoredName
+}
+
+// New returns an initialised cloud formation creator for
+// a cognito user pool client
+func New(purpose, environment, repository, callBackURL string, userPool cfn.NameReferencer) *UserPoolClient {
+	return &UserPoolClient{
+		StoredName:  fmt.Sprintf("UserPoolClient%s", purpose),
+		CallBackURL: callBackURL,
+		Environment: environment,
+		Repository:  repository,
+		ClientName:  fmt.Sprintf("okctl-%s-%s-%s", environment, repository, purpose),
+		UserPool:    userPool,
+		Purpose:     purpose,
+	}
+}
--- pkg/cfn/components/userpoolclient/userpoolclient_test.go
+++ pkg/cfn/components/userpoolclient/userpoolclient_test.go
@@ -1,1 +1,48 @@
-package userpoolclient
+package userpoolclient_test
+
+import (
+	"testing"
+
+	"github.com/oslokommune/okctl/pkg/cfn/components/userpool"
+	"github.com/oslokommune/okctl/pkg/cfn/components/userpoolclient"
+
+	"github.com/awslabs/goformation/v4/cloudformation/cognito"
+
+	"github.com/awslabs/goformation/v4/cloudformation"
+	"github.com/sebdah/goldie/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	up := userpool.New("test", "test")
+
+	testCases := []struct {
+		name     string
+		golden   string
+		resource cloudformation.Resource
+	}{
+		{
+			name:   "Validate output",
+			golden: "user-pool-client.json",
+			resource: userpoolclient.New(
+				"argocd",
+				"test",
+				"test",
+				"https://argocd/callback",
+				up,
+			).Resource(),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := tc.resource.(*cognito.UserPoolClient).MarshalJSON()
+			assert.NoError(t, err)
+
+			g := goldie.New(t)
+			g.Assert(t, tc.golden, got)
+		})
+	}
+}

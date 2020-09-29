007f18826a014c8e1f0c70da703e7a832d72460b
Yngvar Kristiansen <yngvar.kristiansen@byr.oslo.kommune.no>
2020-09-29 13:39:16 +0200

    chore(create): Reword iac

--- cmd/okctl/create.go
+++ cmd/okctl/create.go
@@ -48,7 +48,7 @@ func buildCreateCommand(o *okctl.Okctl) *cobra.Command {
 		Long: `Create various kinds of resources.
 
 Pre-requisites:
-* okctl create infrastructure-as-code files, and assumes that it is
+* okctl creates various configuration files, and assumes that it is
 running in a git repository. Initialize or clone a git repository
 before running any of these commands.`,
 	}

1606c022dbc54b3a96c1fc928e322eda7df8c692
Yngvar Kristiansen <yngvar.kristiansen@byr.oslo.kommune.no>
2020-09-30 10:28:09 +0200

    feat(create): Tell user about git repo requirement (#94)
    Tell user about git repo Having an existing git repo is a prerequisite.

--- cmd/okctl/create.go
+++ cmd/okctl/create.go
@@ -47,6 +47,12 @@ func buildCreateCommand(o *okctl.Okctl) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create commands",
+		Long: `Create various kinds of resources.
+
+Pre-requisites:
+okctl creates various configuration files, and assumes that it is
+running in a git repository. Initialize or clone a git repository
+before running any of these commands.`,
 	}
 
 	cmd.AddCommand(buildCreateClusterCommand(o))

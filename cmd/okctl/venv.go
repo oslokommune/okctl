151ca02397493cb1d44f82a39f6b91ce231c2a22
Yngvar Kristiansen <yngvar.kristiansen@origo.oslo.kommune.no>
2020-11-12 15:03:28 +0100

    feat(venv): Prepare for venv rewrite

--- cmd/okctl/venv.go
+++ cmd/okctl/venv.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"os/exec"
+	"os/user"
 	"regexp"
+	"strings"
+
+	"github.com/oslokommune/okctl/pkg/virtualenv/commandlineprompter"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/logrusorgru/aurora"
+	"github.com/oslokommune/okctl/pkg/commands"
+	"github.com/oslokommune/okctl/pkg/storage"
 
-	cmd "github.com/oslokommune/okctl/pkg/commands"
 	"github.com/oslokommune/okctl/pkg/okctl"
 	"github.com/oslokommune/okctl/pkg/virtualenv"
 	"github.com/spf13/cobra"
@@ -30,9 +36,9 @@ export OKCTL_SHELL=/bin/bash
 okctl venv myenv
 `
 
-// nolint: funlen
+// nolint: gocyclo, funlen, gocognit
 func buildVenvCommand(o *okctl.Okctl) *cobra.Command {
-	opts := virtualenv.VirtualEnvironmentOpts{}
+	okctlEnvironment := commands.OkctlEnvironment{}
 
 	cmd := &cobra.Command{
 		Use:   "venv ENV",
@@ -56,7 +62,7 @@ func buildVenvCommand(o *okctl.Okctl) *cobra.Command {
 				return err
 			}
 
-			opts, err = virtualenv.GetVirtualEnvironmentOpts(o)
+			okctlEnvironment, err = commands.GetOkctlEnvironment(o)
 
 			if err != nil {
 				return err
@@ -65,30 +71,68 @@ func buildVenvCommand(o *okctl.Okctl) *cobra.Command {
 			return nil
 		},
 		RunE: func(_ *cobra.Command, args []string) error {
-			shell := exec.Command(cmd.GetShell(os.LookupEnv)) //nolint:gosec
+			currentUser, err := user.Current()
+			if err != nil {
+				return fmt.Errorf("could not get current user: %w", err)
+			}
 
-			venv, err := virtualenv.GetVirtualEnvironment(&opts, os.Environ())
+			okctlEnvVars := commands.GetOkctlEnvVars(okctlEnvironment)
+			envVars := commands.MergeEnvVars(os.Environ(), okctlEnvVars)
+
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				return fmt.Errorf("could not get user's home directory: %w", err)
+			}
+
+			venvOpts := commandlineprompter.CommandLinePromptOpts{
+				OsEnvVars:          envVars,
+				EtcStorage:         storage.NewFileSystemStorage("/etc"),
+				UserDirStorage:     storage.NewFileSystemStorage(okctlEnvironment.UserDataDir),
+				UserHomeDirStorage: storage.NewFileSystemStorage(homeDir),
+				TmpStorage:         nil,
+				Environment:        okctlEnvironment.Environment,
+				CurrentUsername:    currentUser.Username,
+			}
+
+			tmpStorage, err := storage.NewTemporaryStorage()
 			if err != nil {
 				return err
 			}
 
-			err = printExecutables(venv)
+			venvOpts.TmpStorage = tmpStorage
+
+			venv, err := virtualenv.CreateVirtualEnvironment(venvOpts)
 			if err != nil {
-				return err
+				return fmt.Errorf("could not create virtual environment: %w", err)
 			}
 
-			shell.Env = venv
+			defer func() {
+				err = tmpStorage.Clean()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			err = printWelcomeMessage(o.Out, venv, okctlEnvironment)
+			if err != nil {
+				return fmt.Errorf("could not print welcome message: %w", err)
+			}
+
+			shell := exec.Command(venv.ShellCommand) //nolint:gosec
+			shell.Env = venv.Environ()
 			shell.Stdout = o.Out
 			shell.Stdin = o.In
 			shell.Stderr = o.Err
 
 			err = shell.Run()
-
 			if err != nil {
-				log.Fatalf("Command failed: %v", err)
+				return fmt.Errorf("could not run shell: %w", err)
 			}
 
-			fmt.Println("Exiting okctl virtual environment")
+			_, err = fmt.Fprintln(o.Out, "Exiting okctl virtual environment")
+			if err != nil {
+				return fmt.Errorf("could not print message: %w", err)
+			}
 
 			return nil
 		},
@@ -97,27 +141,79 @@ func buildVenvCommand(o *okctl.Okctl) *cobra.Command {
 	return cmd
 }
 
-func printExecutables(env []string) error {
-	whichKubectl, err := getWhich("kubectl", env)
+type venvWelcomeMessage struct {
+	Environment             string
+	KubectlPath             string
+	AwsIamAuthenticatorPath string
+	CommandPrompt           string
+	VenvCommand             string
+	Warning                 string
+}
+
+func printWelcomeMessage(stdout io.Writer, venv *virtualenv.VirtualEnvironment, opts commands.OkctlEnvironment) error {
+	environ := venv.Environ()
+
+	whichKubectl, err := getWhich("kubectl", environ)
+	if err != nil {
+		return fmt.Errorf("could not get executable 'kubectl': %w", err)
+	}
+
+	whichAwsIamAuthenticator, err := getWhich("aws-iam-authenticator", environ)
+	if err != nil {
+		return fmt.Errorf("could not get executable 'aws-iam-authenticator': %w", err)
+	}
+
+	params := venvWelcomeMessage{
+		Environment:             opts.Environment,
+		KubectlPath:             whichKubectl,
+		AwsIamAuthenticatorPath: whichAwsIamAuthenticator,
+		CommandPrompt:           "<directory> <okctl environment:kubernetes namespace>",
+		VenvCommand:             aurora.Green("okctl venv").String(),
+		Warning:                 venv.Warning,
+	}
+	template := `----------------- OKCTL -----------------
+Environment: {{ .Environment }}
+Using kubectl: {{ .KubectlPath }}
+Using aws-iam-authenticator: {{ .AwsIamAuthenticatorPath }}
+
+Your command prompt now shows
+{{ .CommandPrompt }}
+
+You can override the command prompt by setting the environment variable OKCTL_PS1 before running {{ .VenvCommand }}.
+Or, if you do not want {{ .VenvCommand }} to modify your command prompt at all, set the environment variable
+OKCTL_NO_PS1=true
+`
+
+	if len(venv.Warning) > 0 {
+		template += "\n{{ .Warning }}\n"
+	}
+
+	template += "-----------------------------------------\n"
+
+	msg, err := commands.GoTemplateToString(template, params)
 	if err != nil {
 		return err
 	}
 
-	whichAwsIamAuthenticator, err := getWhich("aws-iam-authenticator", env)
+	_, err = fmt.Fprint(stdout, msg)
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Using kubectl: %s", whichKubectl)
-	fmt.Printf("Using aws-iam-authenticator: %s", whichAwsIamAuthenticator)
-	fmt.Println()
 
 	return nil
 }
 
-func getWhich(cmd string, env []string) ([]byte, error) {
+func getWhich(cmd string, env []string) (string, error) {
 	c := exec.Command("which", cmd) //nolint:gosec
 	c.Env = env
 
-	return c.Output()
+	o, err := c.Output()
+	if err != nil {
+		return "", err
+	}
+
+	which := string(o)
+	which = strings.TrimSpace(which)
+
+	return which, nil
 }
--- pkg/commands/venv.go
+++ pkg/commands/venv.go
@@ -1,16 +1,168 @@
 package commands
 
-// GetShell detects which shell to run, and returns the command to run it.
-func GetShell(osLookupEnv func(key string) (string, bool)) string {
-	shell, ok := osLookupEnv("OKCTL_SHELL")
-	if ok {
-		return shell
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/mishudark/errors"
+	"github.com/oslokommune/okctl/pkg/binaries/run/awsiamauthenticator"
+	"github.com/oslokommune/okctl/pkg/binaries/run/kubectl"
+	"github.com/oslokommune/okctl/pkg/config"
+	"github.com/oslokommune/okctl/pkg/helm"
+	"github.com/oslokommune/okctl/pkg/okctl"
+)
+
+// OkctlEnvironment contains data about the okctl environment
+type OkctlEnvironment struct {
+	Region                 string
+	AWSAccountID           string
+	Environment            string
+	Repository             string
+	ClusterName            string
+	UserDataDir            string
+	Debug                  bool
+	KubectlBinaryDir       string
+	AwsIamAuthenticatorDir string
+}
+
+// Validate the inputs
+func (o *OkctlEnvironment) Validate() error {
+	return validation.ValidateStruct(o,
+		validation.Field(&o.Environment, validation.Required),
+		validation.Field(&o.AWSAccountID, validation.Required),
+		validation.Field(&o.Region, validation.Required),
+		validation.Field(&o.ClusterName, validation.Required),
+		validation.Field(&o.UserDataDir, validation.Required),
+		validation.Field(&o.KubectlBinaryDir, validation.Required),
+		validation.Field(&o.AwsIamAuthenticatorDir, validation.Required),
+	)
+}
+
+// GetOkctlEnvironment returns data needed to connect to an okctl cluster
+func GetOkctlEnvironment(o *okctl.Okctl) (OkctlEnvironment, error) {
+	meta := o.RepoStateWithEnv.GetMetadata()
+	cluster := o.RepoStateWithEnv.GetCluster()
+
+	userDataDir, err := o.GetUserDataDir()
+	if err != nil {
+		return OkctlEnvironment{}, err
 	}
 
-	shell, ok = osLookupEnv("SHELL")
-	if ok {
-		return shell
+	k, err := o.BinariesProvider.Kubectl(kubectl.Version)
+	if err != nil {
+		return OkctlEnvironment{}, err
 	}
 
-	return "/bin/sh"
+	a, err := o.BinariesProvider.AwsIamAuthenticator(awsiamauthenticator.Version)
+	if err != nil {
+		return OkctlEnvironment{}, err
+	}
+
+	opts := OkctlEnvironment{
+		Region:                 meta.Region,
+		AWSAccountID:           cluster.AWSAccountID,
+		Environment:            cluster.Environment,
+		Repository:             meta.Name,
+		ClusterName:            cluster.Name,
+		UserDataDir:            userDataDir,
+		Debug:                  o.Debug,
+		KubectlBinaryDir:       path.Dir(k.BinaryPath),
+		AwsIamAuthenticatorDir: path.Dir(a.BinaryPath),
+	}
+
+	err = opts.Validate()
+	if err != nil {
+		return OkctlEnvironment{}, errors.E(err, "failed to validate show credentials options")
+	}
+
+	return opts, nil
+}
+
+// GetOkctlEnvVars converts an okctl environment to a map with environmental variables
+func GetOkctlEnvVars(opts OkctlEnvironment) map[string]string {
+	appDir := opts.UserDataDir
+
+	kubeConfig := path.Join(appDir, config.DefaultCredentialsDirName, opts.ClusterName, config.DefaultClusterKubeConfig)
+	awsConfig := path.Join(appDir, config.DefaultCredentialsDirName, opts.ClusterName, config.DefaultClusterAwsConfig)
+	awsCredentials := path.Join(appDir, config.DefaultCredentialsDirName, opts.ClusterName, config.DefaultClusterAwsCredentials)
+
+	h := &helm.Config{
+		HelmPluginsDirectory: path.Join(appDir, config.DefaultHelmBaseDir, config.DefaultHelmPluginsDirectory),
+		HelmRegistryConfig:   path.Join(appDir, config.DefaultHelmBaseDir, config.DefaultHelmRegistryConfig),
+		HelmRepositoryCache:  path.Join(appDir, config.DefaultHelmBaseDir, config.DefaultHelmRepositoryCache),
+		HelmRepositoryConfig: path.Join(appDir, config.DefaultHelmBaseDir, config.DefaultHelmRepositoryConfig),
+		HelmBaseDir:          path.Join(appDir, config.DefaultHelmBaseDir),
+		Debug:                opts.Debug,
+	}
+
+	envMap := make(map[string]string)
+
+	for k, v := range h.Envs() {
+		if k == "HOME" || k == "PATH" {
+			continue
+		}
+
+		envMap[k] = v
+	}
+
+	envMap["AWS_CONFIG_FILE"] = awsConfig
+	envMap["AWS_SHARED_CREDENTIALS_FILE"] = awsCredentials
+	envMap["AWS_PROFILE"] = "default"
+	envMap["KUBECONFIG"] = kubeConfig
+	envMap["PATH"] = getPathWithOkctlBinaries(opts)
+
+	return envMap
+}
+
+func getPathWithOkctlBinaries(opts OkctlEnvironment) string {
+	okctlPath := fmt.Sprintf("%s:%s", opts.KubectlBinaryDir, opts.AwsIamAuthenticatorDir)
+	osPath, osPathExists := os.LookupEnv("PATH")
+
+	if osPathExists {
+		return fmt.Sprintf("%s:%s", okctlPath, osPath)
+	}
+
+	return okctlPath
+}
+
+// MergeEnvVars first converts the given slice to a map. The provided slice must contain strings on the form "KEY=VALUE.
+// It then megres this map with the other provided map.
+// If both map contains a PATH key, they will be merged.
+func MergeEnvVars(osEnvs []string, venvMap map[string]string) map[string]string {
+	merged := make(map[string]string)
+
+	for key, val := range venvMap {
+		merged[key] = val
+	}
+
+	osEnvMap := toMap(osEnvs)
+	for key, val := range osEnvMap {
+		merged[key] = val
+	}
+
+	// Merge PATHs
+	venvPath, venvHasPath := venvMap["PATH"]
+	osPath, osHasPath := osEnvMap["PATH"]
+
+	if osHasPath && venvHasPath {
+		merged["PATH"] = fmt.Sprintf("%s:%s", venvPath, osPath)
+	}
+
+	return merged
+}
+
+func toMap(slice []string) map[string]string {
+	m := make(map[string]string)
+
+	for _, env := range slice {
+		split := strings.SplitN(env, "=", 2)
+		key := split[0]
+		val := split[1]
+		m[key] = val
+	}
+
+	return m
 }

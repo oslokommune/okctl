9fd26ff1ccde1a90805bd3c2b295e7fb99085128
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2020-11-19 13:24:13 +0100

    Add cleanup service to delete dangling objects from VPC

---
+++ pkg/api/core/cleanup/service_cleanup.go
@@ -0,0 +1,67 @@
+package cleanup
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/oslokommune/okctl/pkg/api/okctl.io/v1alpha1"
+)
+
+// DeleteDanglingALBs will delete any remaining ALBs in a vpc
+func DeleteDanglingALBs(provider v1alpha1.CloudProvider, vpcID string) error {
+	balancers, err := provider.ELBV2().DescribeLoadBalancers(&elbv2.DescribeLoadBalancersInput{
+		Marker:            nil,
+		PageSize:          nil,
+	})
+	if err != nil {
+		return err
+	}
+
+	loadBalancers := balancers.LoadBalancers
+
+	for _, balancer := range loadBalancers {
+		balancerVPC := *balancer.VpcId
+		if vpcID == balancerVPC {
+			arn := *balancer.LoadBalancerArn
+			fmt.Println(arn)
+			_, err := provider.ELBV2().DeleteLoadBalancer(&elbv2.DeleteLoadBalancerInput{
+				LoadBalancerArn: &arn,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil;
+}
+
+// DeleteDanglingSecurityGroups will remove any remaining security groups in a vpc
+func DeleteDanglingSecurityGroups(provider v1alpha1.CloudProvider, vpcID string) error {
+	groups, err := provider.EC2().DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("vpc-id"),
+				Values: []*string{
+					aws.String(vpcID),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, group := range groups.SecurityGroups {
+		fmt.Println(group.GroupId)
+		provider.EC2().DeleteSecurityGroup(&ec2.DeleteSecurityGroupInput{
+			GroupId:   group.GroupId,
+		})
+
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

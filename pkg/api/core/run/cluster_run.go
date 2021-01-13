4a59ff375ee333ba107fc80ecb8d3739b27cc06a
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2021-01-13 09:10:38 +0100

    KM118: Do not stop if delete fargate profile fails
    There are many things that could fail here, and in most cases eks delete
    cluster will clean up correctly so we should not stop the process.

--- pkg/api/core/run/cluster_run.go
+++ pkg/api/core/run/cluster_run.go
@@ -121,10 +121,9 @@ func (c *clusterRun) DeleteCluster(opts api.ClusterDeleteOpts) error {
 		return fmt.Errorf("failed to retrieve eksctl binary: %w", err)
 	}
 
-	_, err = cli.DeleteFargateProfiles(opts.ID.ClusterName, opts.FargateProfileName)
-	if err != nil {
-		return fmt.Errorf("failed to delete fargate profiles: %w", err)
-	}
+	// We want to continue even if delete fargate profiles fails,
+	// delete cluster should succeed if fp-default is not found
+	_, _ = cli.DeleteFargateProfiles(opts.ID.ClusterName, opts.FargateProfileName)
 
 	_, err = cli.DeleteCluster(opts.ID.ClusterName)
 	if err != nil {

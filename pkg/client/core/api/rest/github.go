f831b9d74bde787fda0b70a87928f8cba2f3e2bc
Julius Pedersen <deifyed@ctemplar.com>
2021-02-11 11:01:33 +0100

    👌 Improve infer repository error

--- pkg/client/core/api/rest/github.go
+++ pkg/client/core/api/rest/github.go
@@ -170,7 +170,7 @@ func inferRepository(fullName string, repositories []*github.Repository) (*githu
 		}
 	}
 
-	return nil, errors.New(fmt.Sprintf("could not find relevant repository %s", fullName))
+	return nil, errors.New(fmt.Sprintf("finding relevant repository: \"%s\"", fullName))
 }
 
 // NewGithubAPI returns an instantiated github API client

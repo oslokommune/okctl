f5f526cda434e24b99ed8a4b5fcf393af760a21e
Paul B. Beskow <paul@beskow.no>
2021-02-19 20:47:30 +0100

    feat(client): add delete of kubepromstack

--- pkg/client/core/state/kubepromstack_state.go
+++ pkg/client/core/state/kubepromstack_state.go
@@ -3,6 +3,8 @@ package state
 import (
 	"fmt"
 
+	"github.com/oslokommune/okctl/pkg/api"
+
 	"github.com/oslokommune/okctl/pkg/client"
 	"github.com/oslokommune/okctl/pkg/client/store"
 	"github.com/oslokommune/okctl/pkg/config/state"
@@ -12,17 +14,18 @@ type kubePromStackState struct {
 	state state.RepositoryStateWithEnv
 }
 
+func (s *kubePromStackState) RemoveKubePromStack(id api.ID) (*store.Report, error) {
+	m := s.state.GetMonitoring()
+	m.DashboardURL = ""
+
+	return s.state.SaveMonitoring(m)
+}
+
 func (s *kubePromStackState) SaveKubePromStack(kube *client.KubePromStack) (*store.Report, error) {
 	m := s.state.GetMonitoring()
-
 	m.DashboardURL = fmt.Sprintf("https://%s", kube.Hostname)
 
-	report, err := s.state.SaveMonitoring(m)
-	if err != nil {
-		return nil, err
-	}
-
-	return report, nil
+	return s.state.SaveMonitoring(m)
 }
 
 // NewKubePromStackState returns an initialised state layer

2c301e7c860ee80800800569118477dc7bac819f
Julius Pedersen <deifyed@ctemplar.com>
2021-02-19 18:25:04 +0100

    👌 Remove ReconciliationManager and add NodeType() to Reconcilers

--- pkg/controller/reconciler/reconciler.go
+++ pkg/controller/reconciler/reconciler.go
@@ -2,8 +2,6 @@
 package reconciler
 
 import (
-	"fmt"
-
 	"github.com/oslokommune/okctl/pkg/controller/resourcetree"
 )
 
@@ -15,49 +13,9 @@ type ReconcilationResult struct {
 
 // Reconciler defines functions needed for the controller to use a reconciler
 type Reconciler interface {
+	NodeType() resourcetree.ResourceNodeType
 	// Reconcile knows how to do what is necessary to ensure the desired state is achieved
 	Reconcile(*resourcetree.ResourceNode) (*ReconcilationResult, error)
 	// SetCommonMetadata knows how to store metadata needed by the reconciler for later use
 	SetCommonMetadata(metadata *resourcetree.CommonMetadata)
-}
-
-// Manager provides a simpler way to organize reconcilers
-type Manager struct {
-	commonMetadata *resourcetree.CommonMetadata
-	Reconcilers    map[resourcetree.ResourceNodeType]Reconciler
-}
-
-// AddReconciler makes a Reconciler available in the Manager
-func (manager *Manager) AddReconciler(key resourcetree.ResourceNodeType, reconciler Reconciler) {
-	reconciler.SetCommonMetadata(manager.commonMetadata)
-
-	manager.Reconcilers[key] = reconciler
-}
-
-// Reconcile chooses the correct reconciler to use based on a nodes type
-func (manager *Manager) Reconcile(node *resourcetree.ResourceNode) (result *ReconcilationResult, err error) {
-	spinner := manager.commonMetadata.Spin
-
-	err = spinner.Start(resourcetree.ResourceNodeTypeToString(node.Type))
-	if err != nil {
-		return nil, fmt.Errorf("starting subspinner: %w", err)
-	}
-
-	defer func() {
-		_ = spinner.Stop()
-	}()
-
-	node.RefreshState()
-
-	return manager.Reconcilers[node.Type].Reconcile(node)
-}
-
-// NewReconcilerManager creates a new Manager with a NoopReconciler already installed
-func NewReconcilerManager(metadata *resourcetree.CommonMetadata) *Manager {
-	return &Manager{
-		commonMetadata: metadata,
-		Reconcilers: map[resourcetree.ResourceNodeType]Reconciler{
-			resourcetree.ResourceNodeTypeGroup: &NoopReconciler{},
-		},
-	}
 }

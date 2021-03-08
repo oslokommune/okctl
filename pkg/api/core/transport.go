4b6942212a1818b14a52bdea0699d94327812009
Paul B. Beskow <paul@beskow.no>
2021-03-08 19:46:43 +0100

    feat(transport): generic struct decoder

--- pkg/api/core/transport.go
+++ pkg/api/core/transport.go
@@ -3,8 +3,10 @@ package core
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
+	kit "github.com/go-kit/kit/transport/http"
 	"github.com/oslokommune/okctl/pkg/api"
 )
 
@@ -17,4 +19,17 @@ func decodeIDRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	}
 
 	return id, nil
+}
+
+// decodeStructRequest can be expanded in the future to act on a given type
+// of `Accept` header, e.g., for marshalling from yaml or other formats.
+func decodeStructRequest(v interface{}) kit.DecodeRequestFunc {
+	return func(_ context.Context, r *http.Request) (interface{}, error) {
+		err := json.NewDecoder(r.Body).Decode(v)
+		if err != nil {
+			return nil, fmt.Errorf("decoding request as json: %w", err)
+		}
+
+		return v, nil
+	}
 }

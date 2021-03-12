9bcdb04bc9822837ff8d9284684be379b5c53bbb
Paul B. Beskow <paul@beskow.no>
2021-03-12 13:45:31 +0100

    feat(cfn): add secret target attachment component

---
+++ pkg/cfn/components/secrettargetattachment/secrettargetattachment_test.go
@@ -0,0 +1,24 @@
+package secrettargetattachment_test
+
+import (
+	"testing"
+
+	"github.com/oslokommune/okctl/pkg/cfn/components/secrettargetattachment"
+	tstr "github.com/oslokommune/okctl/pkg/cfn/components/testing"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []tstr.CloudFormationTemplateTestCase{
+		{
+			Name:   "RDSInstanceSecretTargetAttachment",
+			Golden: "rdsinstance-secretattachment.json",
+			Content: secrettargetattachment.NewRDSDBInstance(
+				"myAttachment",
+				tstr.NewNameReferencer("mySecret"),
+				tstr.NewNameReferencer("myRDSInstance"),
+			),
+		},
+	}
+
+	tstr.RunTests(t, testCases)
+}

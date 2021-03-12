9874036dfb39f5e7f02cad0b96a940d9b70370fc
Paul B. Beskow <paul@beskow.no>
2021-03-12 13:41:52 +0100

    feat(cfn): add iam role component

---
+++ pkg/cfn/components/role/role_test.go
@@ -0,0 +1,40 @@
+package role_test
+
+import (
+	"testing"
+
+	"github.com/oslokommune/okctl/pkg/apis/okctl.io/v1alpha1"
+	"github.com/oslokommune/okctl/pkg/cfn/components/policydocument"
+	"github.com/oslokommune/okctl/pkg/cfn/components/role"
+	tstr "github.com/oslokommune/okctl/pkg/cfn/components/testing"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []tstr.CloudFormationTemplateTestCase{
+		{
+			Name:   "IAM Role",
+			Golden: "role.json",
+			Content: role.New(
+				"myIAMRole",
+				v1alpha1.PermissionsBoundaryARN("123456789012"),
+				[]string{"arn:::policy/some-policy"},
+				policydocument.PolicyDocument{
+					Version: policydocument.Version,
+					Statement: []policydocument.StatementEntry{
+						{
+							Effect: policydocument.EffectTypeAllow,
+							Action: []string{
+								"someAction",
+							},
+							Resource: []string{
+								"*",
+							},
+						},
+					},
+				},
+			),
+		},
+	}
+
+	tstr.RunTests(t, testCases)
+}

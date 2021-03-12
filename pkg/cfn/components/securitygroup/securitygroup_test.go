21597f16c8cf3ed70001e54363a25bb0406e13d4
Paul B. Beskow <paul@beskow.no>
2021-03-12 13:43:36 +0100

    feat(cfn): add postgres incoming/outgoing security groups components

---
+++ pkg/cfn/components/securitygroup/securitygroup_test.go
@@ -0,0 +1,33 @@
+package securitygroup_test
+
+import (
+	"testing"
+
+	"github.com/oslokommune/okctl/pkg/cfn/components/securitygroup"
+	tstr "github.com/oslokommune/okctl/pkg/cfn/components/testing"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []tstr.CloudFormationTemplateTestCase{
+		{
+			Name:   "PostgresIncoming",
+			Golden: "postgres-incoming-sg.json",
+			Content: securitygroup.NewPostgresIncoming(
+				"myIncomingPG",
+				"vpcid-r3ufh3",
+				tstr.NewNameReferencer("mySourceSecurityGroup"),
+			),
+		},
+		{
+			Name:   "PostgresOutgoing",
+			Golden: "postgres-outgoing-sg.json",
+			Content: securitygroup.NewPostgresOutgoing(
+				"myIncomingPG",
+				"vpcid-wof03ef3",
+				[]string{"192.168.1.0/20", "192.168.2.0/20"},
+			),
+		},
+	}
+
+	tstr.RunTests(t, testCases)
+}

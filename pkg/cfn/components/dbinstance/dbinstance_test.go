ab017c1d03b3b21bc59d14fe9fb130e9c057d6f5
Paul B. Beskow <paul@beskow.no>
2021-03-12 13:45:00 +0100

    feat(cfn): add rds db instance component for postgres

---
+++ pkg/cfn/components/dbinstance/dbinstance_test.go
@@ -0,0 +1,29 @@
+package dbinstance_test
+
+import (
+	"testing"
+
+	tstr "github.com/oslokommune/okctl/pkg/cfn/components/testing"
+
+	"github.com/oslokommune/okctl/pkg/cfn/components/dbinstance"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []tstr.CloudFormationTemplateTestCase{
+		{
+			Name:   "Template for DBInstance",
+			Golden: "postgres.json",
+			Content: dbinstance.New(
+				"myPostgresDatabase",
+				"databaseName",
+				"myDbSubnetGroupName",
+				tstr.NewNameReferencer("myDbParameterGroup"),
+				tstr.NewNameReferencer("myMonitoringRole"),
+				tstr.NewNameReferencer("myAdminSecret"),
+				tstr.NewNameReferencer("mySecurityGroup"),
+			),
+		},
+	}
+
+	tstr.RunTests(t, testCases)
+}

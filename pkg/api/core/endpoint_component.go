f613991bd762e53b9f5e0446525b7615bfec8f11
Paul B. Beskow <paul@beskow.no>
2021-03-14 23:16:14 +0100

    feat(api): add ability to create and delete s3 bucket

--- pkg/api/core/endpoint_component.go
+++ pkg/api/core/endpoint_component.go
@@ -18,3 +18,15 @@ func makeDeletePostgresDatabaseEndpoint(s api.ComponentService) endpoint.Endpoin
 		return Empty{}, s.DeletePostgresDatabase(ctx, request.(*api.DeletePostgresDatabaseOpts))
 	}
 }
+
+func makeCreateS3BucketEndpoint(s api.ComponentService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		return s.CreateS3Bucket(ctx, request.(*api.CreateS3BucketOpts))
+	}
+}
+
+func makeDeleteS3BucketEndpoint(s api.ComponentService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		return Empty{}, s.DeleteS3Bucket(ctx, request.(*api.DeleteS3BucketOpts))
+	}
+}

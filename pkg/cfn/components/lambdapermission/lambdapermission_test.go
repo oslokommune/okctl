7bfba281b46b49c1a5c7c08839a690e5167af57f
Paul B. Beskow <paul@beskow.no>
2021-03-15 19:39:48 +0100

    feat(cfn): add support for lambda permission

---
+++ pkg/cfn/components/lambdapermission/lambdapermission_test.go
@@ -0,0 +1,23 @@
+package lambdapermission_test
+
+import (
+	"testing"
+
+	"github.com/oslokommune/okctl/pkg/cfn/components/lambdapermission"
+	tstr "github.com/oslokommune/okctl/pkg/cfn/components/testing"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []tstr.CloudFormationTemplateTestCase{
+		{
+			Name:   "LambdaPermission",
+			Golden: "lambda-permission.yaml",
+			Content: lambdapermission.NewRotateLambdaPermission(
+				"myPermission",
+				tstr.NewNameReferencer("myFunction"),
+			),
+		},
+	}
+
+	tstr.RunTests(t, testCases)
+}

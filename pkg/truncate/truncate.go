1b11c7015fbb959ed54931aef752b1d2619cf330
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-04-10 08:59:35 +0200

    🐛 Fix truncate log not working (#454)

--- pkg/truncate/truncate.go
+++ pkg/truncate/truncate.go
@@ -3,8 +3,14 @@ package truncate
 
 import "fmt"
 
+const truncateFormat = "XXXtruncated%dbytesXXX"
+
 // String truncates a string to the minimum of its length and the given max length
 func String(s *string, maxLength int) string {
+	if s == nil {
+		return ""
+	}
+
 	// This way of doing substrings assumes ASCII and doesn't support UTF-8.
 	// See https://stackoverflow.com/a/56129336
 	truncateLength := min(maxLength, len(*s))
@@ -12,7 +18,7 @@ func String(s *string, maxLength int) string {
 
 	if len(*s) > truncateLength {
 		bytesTruncated := len(*s) - truncateLength
-		truncated += fmt.Sprintf(" [truncated %d bytes]", bytesTruncated)
+		truncated += fmt.Sprintf(truncateFormat, bytesTruncated)
 	}
 
 	return truncated
@@ -20,13 +26,17 @@ func String(s *string, maxLength int) string {
 
 // Bytes truncates a byte array to the minimum of its length and the given max length
 func Bytes(b []byte, maxLength int) []byte {
+	if b == nil {
+		return []byte{}
+	}
+
 	truncateLength := min(maxLength, len(b))
 
 	truncated := b[:truncateLength]
 
 	if len(b) > truncateLength {
 		bytesTruncated := len(b) - truncateLength
-		truncateInfo := fmt.Sprintf(" [truncated %d bytes]", bytesTruncated)
+		truncateInfo := fmt.Sprintf(truncateFormat, bytesTruncated)
 		truncated = append(truncated, truncateInfo...)
 	}
 

48e4c9b70dfab4dc9ecb012e6201e178ae6831fa
Julius Pedersen <deifyed@tutanota.com>
2021-04-13 14:19:02 +0200

    ✅ Add database configuration to scaffold cluster

--- pkg/commands/scaffoldcluster_test.go
+++ pkg/commands/scaffoldcluster_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/oslokommune/okctl/pkg/apis/okctl.io/v1alpha1"
+
 	"github.com/sebdah/goldie/v2"
 	"github.com/stretchr/testify/assert"
 )
@@ -60,4 +62,82 @@ func TestEnsureValidDefaultTemplate(t *testing.T) {
 
 	err = cluster.Validate()
 	assert.NoError(t, err)
+}
+
+// nolint: funlen
+func TestRetrieveValuesFromTemplate(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		withTemplate string
+
+		expectTrueTest func(v1alpha1.Cluster) bool
+	}{
+		{
+			name: "Should correctly scaffold and parse users",
+
+			withTemplate: `
+              users:
+              - email: test.user@origo.oslo.kommune.no`,
+
+			expectTrueTest: func(c v1alpha1.Cluster) bool {
+				if len(c.Users) != 1 {
+					return false
+				}
+
+				if c.Users[0].Email != "test.user@origo.oslo.kommune.no" {
+					return false
+				}
+
+				return true
+			},
+		},
+		{
+			name: "Should correctly scaffold and parse databases",
+
+			withTemplate: `
+              databases:
+                postgres:
+                - name: test
+                  namespace: testns
+                  user: postgres`,
+
+			expectTrueTest: func(c v1alpha1.Cluster) bool {
+				if len(c.Databases.Postgres) != 1 {
+					return false
+				}
+
+				db := c.Databases.Postgres[0]
+
+				if db.Name != "test" {
+					return false
+				}
+
+				if db.Namespace != "testns" {
+					return false
+				}
+
+				if db.User != "postgres" {
+					return false
+				}
+
+				return true
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			var buf bytes.Buffer
+
+			_, _ = buf.WriteString(tc.withTemplate)
+
+			cluster, err := InferClusterFromStdinOrFile(&buf, "-")
+			assert.NoError(t, err)
+
+			assert.True(t, tc.expectTrueTest(*cluster))
+		})
+	}
 }

551df14408d83073ab0376dbfd24051ce42f0d38
Julius Pedersen <deifyed@tutanota.com>
2021-08-04 09:10:13 +0200

    👌 Add timeout type to timeout error to accommodate requeue functionality

--- pkg/api/core/run/kube_run.go
+++ pkg/api/core/run/kube_run.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	merrors "github.com/mishudark/errors"
+
 	v1 "github.com/oslokommune/okctl/pkg/kube/externalsecret/api/types/v1"
 
 	"github.com/oslokommune/okctl/pkg/kube/manifests/scale"
@@ -274,7 +276,7 @@ func (k *kubeRun) CreateExternalDNSKubeDeployment(opts api.CreateExternalDNSKube
 
 	err = client.Watch(resources, 4*time.Minute) // nolint: gomnd
 	if err != nil {
-		return nil, fmt.Errorf("failed while waiting for resources to be created: %w", err)
+		return nil, merrors.E(err, "failed while waiting for resources to be created", merrors.Timeout)
 	}
 
 	deployment, err := yaml.Marshal(ext.DeploymentManifest())

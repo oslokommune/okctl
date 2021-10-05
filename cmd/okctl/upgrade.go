c96f88e029a618eced5b94e52afb140cb9806962
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-10-05 11:31:04 +0200

    👌 Set Hidden: true

--- cmd/okctl/upgrade.go
+++ cmd/okctl/upgrade.go
@@ -37,6 +37,7 @@ func buildUpgradeCommand(o *okctl.Okctl) *cobra.Command {
 		Long: `Runs a series of upgrade migrations to upgrade resources made by okctl
 to the current version of okctl. Example of such resources are helm charts, okctl cluster and application declarations,
 binaries used by okctl (kubectl, etc), and internal state.`,
+		Hidden: true,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			err := o.Initialise()
 			if err != nil {

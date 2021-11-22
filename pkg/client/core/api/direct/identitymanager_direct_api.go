c25291754cf59a132b7ba3f31db56b5a9d8d580f
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2021-11-22 15:00:00 +0100

    KM451: Add direct API for identitymanager

---
+++ pkg/client/core/api/direct/identitymanager_direct_api.go
@@ -0,0 +1,41 @@
+package direct
+
+import (
+	"context"
+	"github.com/oslokommune/okctl/pkg/api"
+	"github.com/oslokommune/okctl/pkg/client"
+)
+
+type identityManagerDirectClient struct {
+	service api.IdentityManagerService
+}
+
+func (i identityManagerDirectClient) CreateIdentityPool(opts api.CreateIdentityPoolOpts) (*api.IdentityPool, error) {
+	return i.service.CreateIdentityPool(context.Background(), opts)
+}
+
+func (i identityManagerDirectClient) DeleteIdentityPool(opts api.DeleteIdentityPoolOpts) error {
+	return i.service.DeleteIdentityPool(context.Background(), opts)
+}
+
+func (i identityManagerDirectClient) CreateIdentityPoolClient(opts api.CreateIdentityPoolClientOpts) (*api.IdentityPoolClient, error) {
+	return i.service.CreateIdentityPoolClient(context.Background(), opts)
+}
+
+func (i identityManagerDirectClient) DeleteIdentityPoolClient(opts api.DeleteIdentityPoolClientOpts) error {
+	return i.service.DeleteIdentityPoolClient(context.Background(), opts)
+}
+
+func (i identityManagerDirectClient) CreateIdentityPoolUser(opts api.CreateIdentityPoolUserOpts) (*api.IdentityPoolUser, error) {
+	return i.service.CreateIdentityPoolUser(context.Background(), opts)
+}
+
+func (i identityManagerDirectClient) DeleteIdentityPoolUser(opts api.DeleteIdentityPoolUserOpts) error {
+	return i.service.DeleteIdentityPoolUser(context.Background(), opts)
+}
+
+func NewIdentityManagerAPI(service api.IdentityManagerService) client.IdentityManagerAPI {
+	return &identityManagerDirectClient{
+		service: service,
+	}
+}

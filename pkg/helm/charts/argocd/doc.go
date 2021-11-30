63c4178a71b656f5e9abe7bfbd099d0b94f86264
Julius Pedersen <deifyed@tutanota.com>
2021-11-30 15:36:54 +0100

    ✅ Add links to chart and values file for ArgoCD

---
+++ pkg/helm/charts/argocd/doc.go
@@ -0,0 +1,3 @@
+// Package argocd exposes the helm chart found here: https://github.com/argoproj/argo-helm/tree/master/charts/argo-cd
+// with the associated values file found here: https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/values.yaml
+package argocd

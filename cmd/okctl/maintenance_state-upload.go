b4160353558e65fe2aaa64bd1c25fb99e2cae9e3
Julius Pedersen <deifyed@tutanota.com>
2022-01-03 08:27:50 +0100

    👌 Improve state upload command usage

--- cmd/okctl/maintenance_state-upload.go
+++ cmd/okctl/maintenance_state-upload.go
@@ -14,7 +14,7 @@ import (
 
 func buildMaintenanceStateUploadCommand(o *okctl.Okctl) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "state-upload",
+		Use:   "state-upload <path to cluster.yaml>",
 		Short: "uploads a state.db",
 		Long:  longMaintenanceStateUploadDescription,
 		Args:  cobra.ExactArgs(1),

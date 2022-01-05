ad5b6884a6662221402093f277e899d10f811513
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-01-05 09:32:48 +0100

    🐛 Refer to state.db in state-upload description (#839)

--- cmd/okctl/maintenance_state-upload.go
+++ cmd/okctl/maintenance_state-upload.go
@@ -14,7 +14,7 @@ import (
 
 func buildMaintenanceStateUploadCommand(o *okctl.Okctl) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "state-upload <path to cluster.yaml>",
+		Use:   "state-upload <path to state.db>",
 		Short: "uploads a state.db",
 		Long:  longMaintenanceStateUploadDescription,
 		Args:  cobra.ExactArgs(1),

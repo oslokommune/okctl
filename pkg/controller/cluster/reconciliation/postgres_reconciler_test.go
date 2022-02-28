9117c6a02ee4dd7b943c743ccacf1aaf63f40f92
Julius Pedersen <deifyed@tutanota.com>
2022-02-28 13:43:22 +0100

    ✅ Add test ensuring db deletion wait on dependent app

--- pkg/controller/cluster/reconciliation/postgres_reconciler_test.go
+++ pkg/controller/cluster/reconciliation/postgres_reconciler_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/oslokommune/okctl/pkg/config/constant"
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/oslokommune/okctl/pkg/apis/okctl.io/v1alpha1"
@@ -96,7 +98,7 @@ func TestPostgresReconciler(t *testing.T) {
 
 			reconciler := NewPostgresReconciler(&mockPostgresService{
 				creationBump: func() { creations++ },
-				deletionBump: func() { deletions++ },
+				deletionBump: func(string) { deletions++ },
 			})
 
 			meta := reconciliation.Metadata{
@@ -120,9 +122,165 @@ func TestPostgresReconciler(t *testing.T) {
 	}
 }
 
+//nolint:funlen
+func TestDeletePostgresWithExistingApp(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		withApplications []v1alpha1.Application
+		withExistingDBs  []v1alpha1.ClusterDatabasesPostgres
+
+		expectDeletions int
+	}{
+		{
+			name: "Should do nothing with one application depending on one database",
+
+			withApplications: []v1alpha1.Application{
+				{
+					Postgres: "mockdb",
+				},
+			},
+			withExistingDBs: []v1alpha1.ClusterDatabasesPostgres{
+				{
+					Name:      "mockdb",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+			},
+			expectDeletions: 0,
+		},
+		{
+			name: "Should delete one database when two are configured and only one dependecy",
+
+			withApplications: []v1alpha1.Application{
+				{
+					Postgres: "mockdb",
+				},
+				{
+					Postgres: "",
+				},
+			},
+			withExistingDBs: []v1alpha1.ClusterDatabasesPostgres{
+				{
+					Name:      "mockdb",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+				{
+					Name:      "secondmockdb",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+			},
+			expectDeletions: 1,
+		},
+		{
+			name: "Should delete everything if no apps are dependent",
+
+			withApplications: []v1alpha1.Application{
+				{
+					Postgres: "",
+				},
+				{
+					Postgres: "",
+				},
+			},
+			withExistingDBs: []v1alpha1.ClusterDatabasesPostgres{
+				{
+					Name:      "mockdb",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+				{
+					Name:      "mockdb2",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+				{
+					Name:      "mockdb3",
+					User:      "postgres",
+					Namespace: "dbdetails",
+				},
+			},
+			expectDeletions: 3,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			deletions := 0
+
+			meta := reconciliation.Metadata{
+				Purge: true,
+				ClusterDeclaration: &v1alpha1.Cluster{
+					Databases: &v1alpha1.ClusterDatabases{Postgres: tc.withExistingDBs},
+				},
+			}
+
+			existingDbs := make([]*client.PostgresDatabase, len(tc.withExistingDBs))
+
+			for index, db := range tc.withExistingDBs {
+				existingDbs[index] = &client.PostgresDatabase{
+					ApplicationName: db.Name,
+					UserName:        db.User,
+					Namespace:       db.Namespace,
+				}
+			}
+
+			state := &clientCore.StateHandlers{
+				Vpc:         &mockVPCState{exists: true},
+				Component:   &mockComponentState{databases: existingDbs},
+				Application: &mockPgAppState{applications: tc.withApplications},
+			}
+
+			reconciler := NewPostgresReconciler(&mockPostgresService{
+				deletionBump: func(name string) {
+					newExistingDbs := make([]*client.PostgresDatabase, 0)
+
+					for _, db := range existingDbs {
+						if db.ApplicationName != name {
+							newExistingDbs = append(newExistingDbs, db)
+						} else {
+							deletions++
+						}
+					}
+
+					existingDbs = newExistingDbs
+				},
+			})
+
+			for i := 0; i < constant.DefaultMaxReconciliationRequeues; i++ {
+				result, err := reconciler.Reconcile(context.Background(), meta, state)
+				assert.NoError(t, err)
+
+				if result.Requeue == false {
+					break
+				}
+			}
+
+			assert.Equal(t, tc.expectDeletions, deletions)
+		})
+	}
+}
+
+type mockPgAppState struct {
+	applications []v1alpha1.Application
+}
+
+func (m mockPgAppState) Initialize(v1alpha1.Cluster, string) error { panic("implement me") }
+func (m mockPgAppState) Put(v1alpha1.Application) error            { panic("implement me") }
+func (m mockPgAppState) Get(string) (v1alpha1.Application, error)  { panic("implement me") }
+func (m mockPgAppState) Delete(string) error                       { panic("implement me") }
+func (m mockPgAppState) List() ([]v1alpha1.Application, error) {
+	return m.applications, nil
+}
+
 type mockPostgresService struct {
 	creationBump func()
-	deletionBump func()
+	deletionBump func(string)
 }
 
 func (m mockPostgresService) CreatePostgresDatabase(_ context.Context, _ client.CreatePostgresDatabaseOpts) (*client.PostgresDatabase, error) {
@@ -131,8 +289,8 @@ func (m mockPostgresService) CreatePostgresDatabase(_ context.Context, _ client.
 	return nil, nil
 }
 
-func (m mockPostgresService) DeletePostgresDatabase(_ context.Context, _ client.DeletePostgresDatabaseOpts) error {
-	m.deletionBump()
+func (m mockPostgresService) DeletePostgresDatabase(_ context.Context, opts client.DeletePostgresDatabaseOpts) error {
+	m.deletionBump(opts.ApplicationName)
 
 	return nil
 }
